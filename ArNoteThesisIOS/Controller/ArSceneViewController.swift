ad470032536ebdb197726a83281614a382c55ebf
krujzg97 <47007011+krujzg97@users.noreply.github.com>
2020-10-19 21:13:43 +0200

    shortcode update works

--- ArNoteThesisIOS/Controller/ArSceneViewController.swift
+++ ArNoteThesisIOS/Controller/ArSceneViewController.swift
@@ -25,6 +25,7 @@ class ArSceneViewController: UIViewController, ARSCNViewDelegate, UIPickerViewDe
     var pickerData: [String] = [String]()
     var type : String = ""
     var textNode = SCNNode()
+    var nextShortCode :Int = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,7 +63,15 @@ class ArSceneViewController: UIViewController, ARSCNViewDelegate, UIPickerViewDe
         }
     }
     
-    func addText(at hitResult: ARHitTestResult)
+    private func addText(at hitResult: ARHitTestResult)
+    {
+        let textGeometry = setMyNoteAppearance()
+        addNodeToTheScene(textGeometry: textGeometry, hitResult: hitResult)
+        getNextShortCodeFromTheDb()
+        saveMyNoteDataToDb()
+    }
+    
+    private func setMyNoteAppearance() -> SCNGeometry
     {
         if textMessage.text == ""{textMessage.text = "Missing text.."}
         let textGeometry = SCNText(string: textMessage.text, extrusionDepth: 1.0)
@@ -70,25 +79,41 @@ class ArSceneViewController: UIViewController, ARSCNViewDelegate, UIPickerViewDe
         if type == "Normal" {textGeometry.firstMaterial?.diffuse.contents = UIColor.black}
         else if type == "Warning"{textGeometry.firstMaterial?.diffuse.contents = UIColor.yellow}
         else{textGeometry.firstMaterial?.diffuse.contents = UIColor.red}
-        
+        return textGeometry
+    }
+    
+    private func addNodeToTheScene(textGeometry : SCNGeometry, hitResult : ARHitTestResult)
+    {
         textNode = SCNNode(geometry: textGeometry)
         textNode.position = SCNVector3(x: hitResult.worldTransform.columns.3.x, y: hitResult.worldTransform.columns.3.y + 0.01, z: hitResult.worldTransform.columns.3.z)
         textNode.scale = SCNVector3(0.01, 0.01, 0.01)
         sceneView.scene.rootNode.addChildNode(textNode)
-        
+    }
+    
+    private func createDbFormat() -> NSDictionary
+    {
         var text_message = textMessage.text
-        let myNoteDb = Database.database().reference().child("MyNotes")
         if text_message == "" {text_message = "Missing Text...."}
         let df = DateFormatter()
         df.dateFormat = "yyyy-MM-dd hh:mm:ss"
         let now = df.string(from: Date())
         let myNoteData = MyNote(
-            shortCode: "11",
+            shortCode: String(nextShortCode+1),
             type: type,
             date: now,
             textMessage: text_message!)
         
-        let dictionary = ["shortcode" : myNoteData.shortcode, "type" : myNoteData.type, "date" : myNoteData.date, "textmessage" : myNoteData.textMessage]
+        return [
+            "shortcode" : myNoteData.shortcode,
+            "type" : myNoteData.type,
+            "date" : myNoteData.date,
+            "textmessage" : myNoteData.textMessage]
+    }
+    
+    private func saveMyNoteDataToDb()
+    {
+        let myNoteDb = Database.database().reference().child("MyNotes")
+        let dictionary = createDbFormat()
         
         myNoteDb.childByAutoId().setValue(dictionary)
         {
@@ -103,6 +128,19 @@ class ArSceneViewController: UIViewController, ARSCNViewDelegate, UIPickerViewDe
             }
         }
     }
+    
+    private func getNextShortCodeFromTheDb()
+    {
+        let shortCodeDb = Database.database().reference().child("ShortCode")
+            
+        shortCodeDb.observe(.value) { (snapshot) in
+            let snapshotValue = snapshot.value as? NSDictionary
+            self.nextShortCode = snapshotValue!["code"] as! Int
+        }
+        
+        shortCodeDb.updateChildValues(["code" : nextShortCode + 1])
+    }
+    
     
     @IBAction func applyFilters(_ sender: UIButton){controllView.isHidden = true}
     @IBAction func cancelFilters(_ sender: UIButton){controllView.isHidden = true}
